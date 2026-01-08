359d3288551077f4280fe53a3e0a646b76504935
Elliott Bache <elliottbache@gmail.com>
2026-01-08 13:26:00 +0100

    feat(cli): add main function with test

--- src/marketflows/cli.py
+++ src/marketflows/cli.py
@@ -1,2 +1,10 @@
+def main() -> int:
+    pass  # eraseme once this function has logic in it
+    pass  # eraseme once this function has logic in it
+    pass  # eraseme once this function has logic in it
+
+    return 0
+
+
 if __name__ == "__main__":
     raise SystemExit(main())
