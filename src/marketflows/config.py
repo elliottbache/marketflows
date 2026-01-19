def0f873d3518c7ff3fa423fb63e102224675c7a
Elliott Bache <elliott.bache@gmail.com>
2026-01-19 13:35:54 +0100

    refactor: add ProviderConfig dataclass
    ProviderConfig groups together variables defined in config.  A default number of narrative tokens is also defined in coingecko.py to define how many coins within each narrative are queried.  accept": "application/json" is also added to the headers.  Finally, an info message is added to the logger for the data that is queried.

--- src/marketflows/config.py
+++ src/marketflows/config.py
@@ -1,3 +1,28 @@
+from dataclasses import dataclass
+
+from marketflows.types import FlowType
+
+@dataclass
+class ProviderConfig:
+    """Create a dataclass to hold the configuration values for the providers.
+
+    Attributes:
+        days (int): the number of days we want to collect data for
+        flow_types (list[FlowType]): the list of flow types we want to collect data for
+        base_assets (list[str]): assets used as base currency (e.g. EUR, JPY, etc.).
+            These must be in decreasing MC order.
+        narratives (list[str]): list of narratives to be graphed
+        range_lower_limits (list[float]): the lower limits of each of the ranges
+        asset_groups (dict[str, list[str]]): groups of assets as would be the case for multiple portfolios
+            containing (often) different assets
+    """
+    days: int
+    flow_types: list[FlowType]
+    base_assets: list[str]
+    narratives: list[str]
+    range_lower_limits: list[float]
+    asset_groups: dict[str, list[str]]
+
 # function to tie all other config functions together: load_and_validate_config
 
 # function read config file
