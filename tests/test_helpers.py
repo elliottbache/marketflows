9168a6f64862fd8d672ca4a3fd3f44c1bf073001
Elliott Bache <elliott.bache@gmail.com>
2026-02-18 11:49:29 +0100

    refactor(helpers): improve after code review

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,50 +6,44 @@ from marketflows import _helpers
 
 class TestFindFirstValidTime:
     def test_find_first_valid_time_success(self):
-        df = pd.DataFrame()
-        df.index = [
-            pd.Timestamp("1970-01-01 00:00:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:10:00+0000", tz="UTC"),
-        ]
-        df["nvidia"] = [np.nan, 900, 1100]
+        idx = pd.date_range(
+            start="1970-01-01 00:00:00+0000", periods=3, freq="5min", tz="UTC"
+        )
+        df = pd.DataFrame({"nvidia": [np.nan, 900, 1100]}, index=idx)
         first_valid = _helpers.find_first_valid_time(df)
         assert first_valid == pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC")
 
     def test_find_first_valid_time_no_valid(self):
-        df = pd.DataFrame()
-        df.index = [
-            pd.Timestamp("1970-01-01 00:00:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:10:00+0000", tz="UTC"),
-        ]
-        df["nvidia"] = [np.nan, np.nan, np.nan]
-        df["amazon"] = [np.nan, np.nan, np.nan]
+        idx = pd.date_range(
+            start="1970-01-01 00:00:00+0000", periods=3, freq="5min", tz="UTC"
+        )
+        df = pd.DataFrame(
+            {"nvidia": [np.nan, np.nan, np.nan], "amazon": [np.nan, np.nan, np.nan]},
+            index=idx,
+        )
         first_valid = _helpers.find_first_valid_time(df)
         assert first_valid is None
 
 
 class TestFindLastValidTime:
     def test_find_last_valid_time_success(self):
-        df = pd.DataFrame()
-        df.index = [
-            pd.Timestamp("1970-01-01 00:00:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:10:00+0000", tz="UTC"),
-        ]
-        df["nvidia"] = [1100, 900, np.nan]
+        idx = pd.date_range(
+            start="1970-01-01 00:00:00+0000", periods=3, freq="5min", tz="UTC"
+        )
+        df = pd.DataFrame(
+            {"nvidia": [1100, 900, np.nan], "tesla": [1101, np.nan, np.nan]}, index=idx
+        )
         last_valid = _helpers.find_last_valid_time(df)
-        assert last_valid == pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC")
+        assert last_valid == pd.Timestamp("1970-01-01 00:00:00+0000", tz="UTC")
 
     def test_find_last_valid_time_no_valid(self):
-        df = pd.DataFrame()
-        df.index = [
-            pd.Timestamp("1970-01-01 00:00:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:05:00+0000", tz="UTC"),
-            pd.Timestamp("1970-01-01 00:10:00+0000", tz="UTC"),
-        ]
-        df["nvidia"] = [np.nan, np.nan, np.nan]
-        df["amazon"] = [np.nan, np.nan, np.nan]
+        idx = pd.date_range(
+            start="1970-01-01 00:00:00+0000", periods=3, freq="5min", tz="UTC"
+        )
+        df = pd.DataFrame(
+            {"nvidia": [np.nan, np.nan, np.nan], "amazon": [np.nan, np.nan, np.nan]},
+            index=idx,
+        )
         last_valid = _helpers.find_last_valid_time(df)
         assert last_valid is None
 
