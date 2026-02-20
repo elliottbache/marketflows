764b919fa17bab54426a35e40befffffa743fa64
Elliott Bache <elliottbache@gmail.com>
2026-02-20 13:44:40 +0100

    feat(tables): add table creation functions

--- src/marketflows/plots/_helpers.py
+++ src/marketflows/plots/_helpers.py
@@ -2,7 +2,7 @@ import logging
 
 import pandas as pd
 
-from marketflows._helpers import find_first_valid_time
+from marketflows._helpers import find_first_valid_time, name_column
 
 _DEFAULT_GROWTH_PERIODS = 50  # periods in growth window
 _DEFAULT_INFLECTION_PERIODS = 12  # periods in inflection window
@@ -73,51 +73,79 @@ def define_graph_origin(
     return graph_origin
 
 
-def _make_group_columns(
-    df: pd.DataFrame,
+def find_last_valid_time(df: pd.DataFrame) -> pd.Timestamp | None:
+    """Find last valid time for given dataframe.
+
+    For time to be valid, all values in a row must be a number.  If one of the columns
+    has no valid data, it will be ignored.
+
+    Examples:
+        >>> import pandas as pd
+        >>> idx = pd.date_range("2020-01-01", periods=3, freq="1s", tz="UTC")
+        >>> df = pd.DataFrame({"a": [1.0, 2.0, None], "b": [1.0, 2.0, 3.0]}, index=idx)
+        >>> _find_last_valid_time(df) == idx[1]
+        True
+    """
+    last_indices = df.apply(lambda x: x.last_valid_index())
+    valid_indices = last_indices.dropna()
+    if valid_indices.empty:
+        return None
+    else:
+        return valid_indices.min()
+
+
+def create_nice_plot_text(
     *,
-    groups: list[str] | None = None,
-    base_asset: str | None = None,
-    ema_period: int | None = None,
-    diff_order: int | None = None,
-    is_unit: bool | None = None,
-) -> list[str]:
-    """Create a sublist of columns that have the same parameters."""
-    reduced_columns = list()
-    for column in df.columns:
-        column_params = _split_column(column)
+    text_type: str,
+    group: str,
+    base_asset: str = "us-dollar",
+    diff_order: int = 0,
+    ema_period: int = 1,
+    smooth_periods: int = 10,
+) -> str:
+    """Create nice plot text for title or file name.
 
-        if groups is not None and column_params["group"] not in groups:
-            continue
-        if (
-            base_asset is not None
-            and "base_asset" in column_params
-            and column_params["base_asset"] != base_asset
-        ):
-            continue
-        if (
-            ema_period is not None
-            and "ema_period" in column_params
-            and column_params["ema_period"] != str(ema_period)
-        ):
-            continue
-        if (
-            diff_order is not None
-            and "diff_order" in column_params
-            and column_params["diff_order"] != str(diff_order)
-        ):
-            continue
-        if is_unit is not None and "is_unit" in column_params:
-            this_is_unit = column_params["is_unit"] == "True"
-            if this_is_unit != is_unit:
-                continue
+    Args:
+        text_type: ``plot_title`` or ``file_name``
+        group: first string in text
+        base_asset: base asset to use
+        diff_order: differentiation order
+        ema_period: EMA periods (not the smoothing EMA periods)
+        smooth_periods: EMA periods used for smoothing after all calculations.  These
+            are not used for ranges since EMA periods are not used before
+            differentiation as for narratives and asset groups.
 
-        reduced_columns.append(column)
+    Returns:
+        string to be used for title or file name
 
-    return reduced_columns
+    Examples:
+        >>> _create_nice_plot_text(text_type="file_name", group="narratives")
+        'narratives_MC_by_us-dollar'
+        >>> _create_nice_plot_text(text_type="plot_title", group="narratives")
+        'narratives MC by us-dollar'
+        >>> _create_nice_plot_text(text_type="file_name", group="narratives", diff_order=1)
+        'narratives_MC_by_us-dollar_growth_smooth10'
+    """
+    plot_text = name_column(
+        original_column="MC",
+        base_asset=base_asset,
+        ema_period=ema_period,
+        diff_order=diff_order,
+    )
+    plot_text = group + "_" + plot_text
+
+    if text_type == "file_name":
+        if diff_order > 0:
+            plot_text += "_smooth" + str(smooth_periods)
+    elif text_type == "plot_title":
+        plot_text = " ".join(plot_text.split("_"))
+    else:
+        raise ValueError("text_type must be plot_title or file_name.")
+
+    return plot_text.strip()
 
 
-def _split_column(column: str) -> dict[str, str]:
+def split_column(column: str) -> dict[str, str]:
     """Take column name and extract its different parameters."""
     substrings = column.split("_")
 
@@ -152,9 +180,53 @@ def _split_column(column: str) -> dict[str, str]:
     return column_params
 
 
+def _make_group_columns(
+    df: pd.DataFrame,
+    *,
+    groups: list[str] | None = None,
+    base_asset: str | None = None,
+    ema_period: int | None = None,
+    diff_order: int | None = None,
+    is_unit: bool | None = None,
+) -> list[str]:
+    """Create a sublist of columns that have the same parameters."""
+    reduced_columns = list()
+    for column in df.columns:
+        column_params = split_column(column)
+
+        if groups is not None and column_params["group"] not in groups:
+            continue
+        if (
+            base_asset is not None
+            and "base_asset" in column_params
+            and column_params["base_asset"] != base_asset
+        ):
+            continue
+        if (
+            ema_period is not None
+            and "ema_period" in column_params
+            and column_params["ema_period"] != str(ema_period)
+        ):
+            continue
+        if (
+            diff_order is not None
+            and "diff_order" in column_params
+            and column_params["diff_order"] != str(diff_order)
+        ):
+            continue
+        if is_unit is not None and "is_unit" in column_params:
+            this_is_unit = column_params["is_unit"] == "True"
+            if this_is_unit != is_unit:
+                continue
+
+        reduced_columns.append(column)
+
+    return reduced_columns
+
+
 def _define_shifted_index(*, df: pd.DataFrame, periods: int) -> pd.Timestamp | None:
     """Shift origin for given periods."""
-    last_time = _find_last_valid_time(df)
+    last_time = find_last_valid_time(df)
     if last_time is None:
         return None
 
@@ -164,24 +236,3 @@ def _define_shifted_index(*, df: pd.DataFrame, periods: int) -> pd.Timestamp | N
     indices = df.index.to_list()
 
     return indices[first_index]
-
-
-def _find_last_valid_time(df: pd.DataFrame) -> pd.Timestamp | None:
-    """Find last valid time for given dataframe.
-
-    For time to be valid, all values in a row must be a number.  If one of the columns
-    has no valid data, it will be ignored.
-
-    Examples:
-        >>> import pandas as pd
-        >>> idx = pd.date_range("2020-01-01", periods=3, freq="1s", tz="UTC")
-        >>> df = pd.DataFrame({"a": [1.0, 2.0, None], "b": [1.0, 2.0, 3.0]}, index=idx)
-        >>> _find_last_valid_time(df) == idx[1]
-        True
-    """
-    last_indices = df.apply(lambda x: x.last_valid_index())
-    valid_indices = last_indices.dropna()
-    if valid_indices.empty:
-        return None
-    else:
-        return valid_indices.min()
---
+++ tests/plots/test_tables.py
@@ -0,0 +1,323 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pytest
+from matplotlib.table import Table
+
+from marketflows.plots import tables as plots_tables
+
+
+@pytest.fixture
+def hours_ago():
+    return [4, 8, 12, 24, 48, 72, 168, 336, 672]
+
+
+def test_create_category_tables(monkeypatch, df_groups):
+    df_groups = df_groups.copy()
+    base_assets = ["us-dollar", "japan-yen", "china-yuan"]
+
+    calls = []
+
+    def mock_create_table(
+        *,
+        category: str,
+        base_asset: str,
+        groups: list[str],
+        df: pd.DataFrame,
+        hours_ago: list[int],
+    ) -> None:
+        calls.append(base_asset)
+
+    monkeypatch.setattr(plots_tables, "_create_table", mock_create_table)
+
+    plots_tables.create_category_tables(
+        category="Narratives",
+        groups=["pharma", "ai"],
+        base_assets=base_assets,
+        df=df_groups,
+    )
+
+    assert len(calls) == len(base_assets)
+    for idx in range(len(calls)):
+        assert calls[idx] == base_assets[idx]
+
+
+class TestCreateTable:
+    def test_create_table_adds_lines(self, df_groups, tmp_path, hours_ago):
+        df_groups = df_groups.copy()
+        df_groups.index = pd.date_range(
+            end=pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+            periods=6,
+            freq="100h",
+        )
+        df_groups = df_groups.rename(
+            columns={
+                "pharma": "pharma_by_us-dollar",
+                "ai": "ai_by_us-dollar",
+            }
+        )
+
+        fig, ax = plt.subplots()
+        out_file = tmp_path / "test.png"
+        _ = plots_tables._create_table(
+            category="Narratives",
+            base_asset="us-dollar",
+            groups=["pharma", "ai"],
+            df=df_groups,
+            hours_ago=hours_ago,
+            ax=ax,
+            out_path=out_file,
+        )
+
+        # assert that a title was added
+        assert ax.get_title() is not None
+
+        # assert that the axes were turned off
+        assert ax.axison is False
+
+        # check that exactly one table was made
+        tables = [child for child in ax.get_children() if isinstance(child, Table)]
+        assert len(tables) == 1
+        the_table = tables[0]
+
+        # verify table size
+        assert (
+            len(the_table.get_celld())
+            == (len(df_groups.columns) + 1) * (len(hours_ago) + 1) - 1
+        )
+
+        plt.close(fig)
+
+    def test_create_table_creates_file(self, df_groups, tmp_path, hours_ago):
+        df_groups = df_groups.copy()
+        df_groups.index = pd.date_range(
+            end=pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+            periods=6,
+            freq="100h",
+        )
+        df_groups = df_groups.rename(
+            columns={"pharma": "pharma_by_us-dollar", "ai": "ai_by_us-dollar"}
+        )
+
+        out_file = tmp_path / "test.png"
+        out_file = plots_tables._create_table(
+            category="Narratives",
+            base_asset="us-dollar",
+            groups=["pharma", "ai"],
+            df=df_groups,
+            hours_ago=hours_ago,
+            out_path=out_file,
+        )
+        assert out_file.exists()
+        assert out_file.stat().st_size > 0
+        assert out_file.suffix == ".png"
+
+
+@pytest.mark.parametrize(
+    "pharma_values, ai_values, pharma_values_exp, ai_values_exp, last_time_exp",
+    [
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, 1500.0],
+            [1001.0, 901.0, 1101.0, 1201.0, 1301.0, 1501.0],
+            [
+                0.53619,
+                1.07817,
+                1.62602,
+                3.30579,
+                6.83761,
+                10.61947,
+                21.75325,
+                45.91440,
+                np.nan,
+            ],
+            [
+                0.53583,
+                1.07744,
+                1.62492,
+                3.30351,
+                6.83274,
+                10.61164,
+                21.73560,
+                45.86978,
+                np.nan,
+            ],
+            pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+        ),
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, 0.0],
+            [1001.0, 901.0, 1101.0, 1201.0, 1301.0, 1501.0],
+            [-100.0, -100.0, -100.0, -100.0, -100.0, -100.0, -100.0, -100.0, np.nan],
+            [
+                0.53583,
+                1.07744,
+                1.62492,
+                3.30351,
+                6.83274,
+                10.61164,
+                21.73560,
+                45.86978,
+                np.nan,
+            ],
+            pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+        ),
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, np.nan],
+            [1001.0, 901.0, 1101.0, 1201.0, 1301.0, 1501.0],
+            [
+                0.308642,
+                0.619195,
+                0.931677,
+                1.880878,
+                3.833866,
+                5.863192,
+                14.840989,
+                38.888889,
+                np.nan,
+            ],
+            [
+                0.3084040092521203,
+                0.6187161639597834,
+                0.9309542280837859,
+                1.8794048551292091,
+                3.830806065442937,
+                5.8584214808787635,
+                14.8278905560459,
+                38.847385272145145,
+                np.nan,
+            ],
+            pd.Timestamp("1970-12-27 20:00:00+0000", tz="UTC"),
+        ),
+        (
+            [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [1001.0, 901.0, 1101.0, 1201.0, 1301.0, 1501.0],
+            [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [
+                0.53583,
+                1.07744,
+                1.62492,
+                3.30351,
+                6.83274,
+                10.61164,
+                21.73560,
+                45.86978,
+                np.nan,
+            ],
+            pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+        ),
+    ],
+    ids=["normal", "no_group", "second_to_last_valid_time", "no_valid_records"],
+)
+def test_calculate_groups_gains(
+    df_groups,
+    pharma_values,
+    ai_values,
+    pharma_values_exp,
+    ai_values_exp,
+    last_time_exp,
+    hours_ago,
+):
+    df_groups = df_groups.copy()
+    df_groups.index = pd.date_range(
+        end=pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"), periods=6, freq="100h"
+    )
+    df_groups["pharma_by_us-dollar"] = pharma_values
+    df_groups["ai_by_us-dollar"] = ai_values
+
+    df_out = plots_tables._calculate_groups_gains(
+        groups=["pharma_by_us-dollar", "ai_by_us-dollar"],
+        df=df_groups,
+        hours_ago=hours_ago,
+    )
+
+    df_exp = pd.DataFrame(
+        data={
+            "pharma_by_us-dollar": pharma_values_exp,
+            "ai_by_us-dollar": ai_values_exp,
+        },
+        index=last_time_exp - pd.to_timedelta(hours_ago, unit="h"),
+        dtype=np.float64,
+    )
+    df_exp = df_exp.dropna(how="all", axis=1)
+    print(f"df_exp: {df_exp}")
+    pd.testing.assert_frame_equal(df_out, df_exp, atol=1e-4, check_names=False)
+
+
+@pytest.mark.parametrize(
+    "pharma_values, last_time, pharma_values_exp",
+    [
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, 1500.0],
+            pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+            [
+                0.53619,
+                1.07817,
+                1.62602,
+                3.30579,
+                6.83761,
+                10.61947,
+                21.75325,
+                45.91440,
+                np.nan,
+            ],
+        ),
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, 0.0],
+            pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"),
+            [-100.0, -100.0, -100.0, -100.0, -100.0, -100.0, -100.0, -100.0, np.nan],
+        ),
+        (
+            [1000.0, 900.0, 1100.0, 1200.0, 1300.0, np.nan],
+            pd.Timestamp("1970-12-27 20:00:00+0000", tz="UTC"),
+            [
+                0.308642,
+                0.619195,
+                0.931677,
+                1.880878,
+                3.833866,
+                5.863192,
+                14.840989,
+                38.888889,
+                np.nan,
+            ],
+        ),
+    ],
+    ids=["normal", "zero_last_value", "missing_last_value"],
+)
+def test_calculate_gains(
+    df_groups, pharma_values, last_time, pharma_values_exp, hours_ago
+):
+    df_groups = df_groups.copy()
+    df_groups.index = pd.date_range(
+        end=pd.Timestamp("1971-01-01 00:00:00+0000", tz="UTC"), periods=6, freq="100h"
+    )
+    df_groups["pharma"] = pharma_values
+
+    # pharma
+    ser_out = plots_tables._calculate_gains(
+        ser=df_groups["pharma"], hours_ago=hours_ago, last_time=last_time
+    )
+    ser_exp = pd.Series(
+        data=pharma_values_exp,
+        index=last_time - pd.to_timedelta(hours_ago, unit="h"),
+        dtype=np.float64,
+    )
+    print(f"ser_exp: {ser_exp}")
+    pd.testing.assert_series_equal(ser_out, ser_exp, atol=1e-4, check_names=False)
+
+
+def test_interpolate_series(df_groups):
+    indexes = df_groups.index - pd.to_timedelta(2, unit="m")
+
+    # pharma
+    ser_out = plots_tables._interpolate_series(indexes=indexes, ser=df_groups["pharma"])
+    ser_exp = pd.Series(
+        data=[np.nan, 940.0, 1020.0, 1160.0, 1260.0, 1420.0], index=indexes
+    )
+    pd.testing.assert_series_equal(ser_out, ser_exp, rtol=1e-6, check_names=False)
+
+    # ai
+    ser_out = plots_tables._interpolate_series(indexes=indexes, ser=df_groups["ai"])
+    ser_exp = pd.Series(
+        data=[np.nan, 941.0, 1021.0, 1161.0, 1261.0, 1421.0], index=indexes
+    )
+    pd.testing.assert_series_equal(ser_out, ser_exp, rtol=1e-6, check_names=False)
